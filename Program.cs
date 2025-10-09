2526b3353daf99e44650531df16056f4f9ea3166
Marcelo <mmarcelohenriquegmh@gmail.com>
2025-10-09 14:32:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class MembroDaSociedade
+{
+    private string nome;
+    private string raca;
+    private string func;
+
+    public MembroDaSociedade(string nome, string raca, string func)
+    {
+        this.nome = nome;
+        this.raca = raca;
+        this.func = func;
+    }
+    
+    public void apresentar()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Raca: {raca}");
+        Console.WriteLine($"função: {func}");
+        Console.WriteLine("__________________");
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MembroDaSociedade aragon = new MembroDaSociedade("Aragon", "Humano", "Guardiao");
+        MembroDaSociedade legolas = new MembroDaSociedade("Legolas", "Elfo", "Arqueiro");
+
+        aragon.apresentar();
+        legolas.apresentar();
+
+        Console.WriteLine("\n=== Fim da Demonstração ==="); 
+    }
+}
